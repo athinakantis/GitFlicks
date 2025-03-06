48814c314f47cd22a05d4982b0c85e52783fc11c
Nikita Luzianin <s2400796@edu.bc.fi>
2025-03-06 12:26:44 +0200

    update added arguments to body

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import './App.css';
 
 import Main from './components/Main';
+import shorten from './utility/shortenerApi';
 
 
 function App() {
 
-
+    shorten("https://facebook.com").then(console.log);
 
     return (
         <Main />
--- src/utility/shortenerApi.ts
+++ src/utility/shortenerApi.ts
@@ -5,7 +5,10 @@ import { send } from "./axios";
 
 export default async function shorten(longUrl: string) {
 
-    const data = { "long_url": longUrl };
+    const data = {
+        "long_url": longUrl,
+        "type": "direct",
+    };
     const headers = { "Authorization": `Bearer ${import.meta.env.VITE_API_KEY}` };
     const APILINK = "https://unelma.io/api/v1/link";
 
