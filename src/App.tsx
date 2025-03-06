02082c125c8189cbcb955fe3bab21a9303abaa80
Nikita Luzianin <s2400796@edu.bc.fi>
2025-03-06 12:29:48 +0200

    update added arguments to body

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import './App.css';
 
 import Main from './components/Main';
-import shorten from './utility/shortenerApi';
 
 
 function App() {
 
-    shorten("https://facebook.com").then(console.log);
 
     return (
         <Main />
