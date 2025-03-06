1c980d72b470560d643310d1ea2b42bfb53991c4
[Athina Kantis] <[athina.kantis@gmail.com]>
2025-03-06 12:34:24 +0200

    update Main component

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,30 +1,41 @@
-import React, { useState } from "react"
-
+import React, { useState } from 'react';
+import Hand from './icons/Hand';
 
 const Main: React.FC = () => {
-    const [originalLink, setOriginalLink] = useState("")
+    const [originalLink, setOriginalLink] = useState('');
 
     const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setOriginalLink(e.target.value)
-        
-        console.log(e.target.value);      
-        
-    }
-  return (
-    <div>
-      <header>
-        <h1>GitFlicks 👌</h1>
-        <p>A simple, free-to-use URL shortener.</p>
-      </header>
-      <form>
-        <label>Paste a URL to shorten</label>
-        <br />
-        <input type="text" value={originalLink} onChange={handleInputChange}/>
-        <br />        
-      <button type="submit" >Shorten</button>
-      </form>
-    </div>
-  )
-}
+        setOriginalLink(e.target.value);
+    };
 
-export default Main
+    return (
+        <main>
+            <div className='content-container'>
+                <div id='logo'>
+                    <h1>GitFlick<span>s</span></h1>
+                    <Hand />
+                    <p>A simple, free-to-use URL shortener.</p>
+                </div>
+                <form>
+                    <div className='input-container'>
+                        <label htmlFor='originalLink'>
+                            Paste a URL to shorten
+                        </label>
+                        <input
+                            className='input-link'
+                            type='text'
+                            value={originalLink}
+                            id='originalLink'
+                            onChange={handleInputChange}
+                        />
+                    </div>
+                    <button type='submit' className='CTA-button'>
+                        <span>Shorten</span>
+                    </button>
+                </form>
+            </div>
+        </main>
+    );
+};
+
+export default Main;
