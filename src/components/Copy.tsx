e74858fd235d80288210f8778cd12076882215ad
[Athina Kantis] <[athina.kantis@gmail.com]>
2025-03-13 13:46:32 +0200

    update copy component

--- src/components/Copy.tsx
+++ src/components/Copy.tsx
@@ -1,24 +1,23 @@
 import { Tooltip } from '@mui/material';
-import { CopyToClipboard } from 'react-copy-to-clipboard';
+import copy from 'react-copy-to-clipboard';
 import CopyIcon from './icons/Copy';
 import { useState } from 'react';
 
 const Copy = ({ shortUrl }: { shortUrl: string }) => {
     const [open, setOpen] = useState(false);
 
-    const handleTooltip = () => {
-        setOpen(prev => !prev)
-        setTimeout(() => setOpen(false), 2000)
-    }
+    const handleClick = () => {
+        setOpen((prev) => !prev);
+        copy(shortUrl);
+        setTimeout(() => setOpen(false), 2000);
+    };
 
     return (
-        <CopyToClipboard text={shortUrl}>
-            <Tooltip title='Copied!' open={open} placement='top'>
-                <button onClick={handleTooltip} aria-label='Copy link'>
-                    <CopyIcon />
-                </button>
-            </Tooltip>
-        </CopyToClipboard>
+        <Tooltip title='Copied!' open={open} placement='top'>
+            <button onClick={handleClick} aria-label='Copy link'>
+                <CopyIcon />
+            </button>
+        </Tooltip>
     );
 };
 
